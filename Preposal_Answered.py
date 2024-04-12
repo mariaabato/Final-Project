cfc54d2860588c95cd0a723ced15a260bc4ccd78
mariaabato <166047296+mariaabato@users.noreply.github.com>
2024-04-11 21:07:03 -0400

    Add files via upload

---
+++ Preposal_Answered.py
@@ -0,0 +1,13 @@
+# Preposal
+
+## What idea(s) do you have for your final project?
+
+#I want to either make a murder mystery visual novel using Renpy and incorporate mini games using python 
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+#N/A
+
+## Do you have any questions of your own?
+
+#N/A
