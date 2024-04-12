48870da02135a0568d0454402cf9a88ffc769378
mariaabato <166047296+mariaabato@users.noreply.github.com>
2024-04-11 21:37:27 -0400

    Add files via upload

---
+++ Proposal_Answered.py
@@ -0,0 +1,39 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+#Maria's super intense and super cool visual novel
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+#A visual novel that has a murder mystery story and minigames throughout in order to find the killer
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+#I will use Renpy to create a murder mystery visual novel. Ideally, the visual novel will include both background and character art, background music, mini games that bring the player closer to the killer, multiple character types that the player can interact with and create relationships with, and jumpscares during key moments of the game.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+#N/A
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+#N/A
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+#A finished visual novel including an ending with background art and at least one minigame
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+#A fully developed visual novel with a satisfying ending, including 2 minigames and background art
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+#A fully developed visual novel with character art, and background art, and music, incorporates 3+ mini games and has a satisfying ending. I would also incorporate jumpscares if i have the time
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+#To make my visual novel, I'll need to familarize myself with Renpy and how exactly I can incorporate mini games into my game. I also need to learn how to impliment music as well.  
